2455c241a28dc68e6de161e235096fa1b8fc4901
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-23 03:37:01 +0300

    fix: подключение к БД

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "postgres",
-    password: "12345",
-    host: "localhost",
+    user: "jrskmdjobstsft",
+    password: "0631d8e816e212e99ad01d5b6f0a52ad7bd6c159ac6e0bf63b659ddbfed25da5",
+    host: "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
     port: "5432",
-    database: "welbex_test"
+    database: "ddhr4mc2han9jd"
 })
 
 module.exports = pool
