3717ecc1aba82bba9b9aade58d988dd7798d4bc9
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-23 03:38:01 +0300

    fix: подключение к БД

--- controller/data.controller.js
+++ controller/data.controller.js
@@ -2,7 +2,7 @@ const db = require('../db')
 
 class DataController {
     async getData(req, res) {
-        const table = await db.query('SELECT * FROM welbex_data')
+        const table = await db.query('SELECT * FROM "public".welbex_data')
         res.json(table.rows)
     }
 }
