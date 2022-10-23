9b5d799b984151efcd61f7e318b8446fd4284682
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-23 04:48:47 +0300

    feat: подключение к БД

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: "jrskmdjobstsft",
-    password: "0631d8e816e212e99ad01d5b6f0a52ad7bd6c159ac6e0bf63b659ddbfed25da5",
-    host: "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
-    port: "5432",
-    database: "ddhr4mc2han9jd"
-})
+const { Client } = require('pg');
 
-module.exports = pool
+const client = new Client({
+  connectionString: 'postgres://jrskmdjobstsft:0631d8e816e212e99ad01d5b6f0a52ad7bd6c159ac6e0bf63b659ddbfed25da5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/ddhr4mc2han9jd',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+module.exports = client
